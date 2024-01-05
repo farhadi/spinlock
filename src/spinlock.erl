864d5d70172391486bcf7575fc7ded91f28de34c
Ali Farhadi <a.farhadi@gmail.com>
2024-01-05 12:50:33 +0100

    Initial commit

---
+++ src/spinlock.erl
@@ -0,0 +1,78 @@
+-module(spinlock).
+
+-export([
+    new/0, new/1,
+    acquire/1,
+    release/2,
+    transaction/2,
+    status/1
+]).
+
+-record(spinlock, {
+    ref :: atomics:atomics_ref(),
+    max_retry :: pos_integer()
+}).
+
+-type option() :: {max_retry, pos_integer()}.
+-type lock_id() :: pos_integer().
+
+-spec new() -> #spinlock{}.
+new() ->
+    new([]).
+
+-spec new([option()]) -> #spinlock{}.
+new(Options) ->
+    MaxRetry = proplists:get_value(max_retry, Options, 100_000),
+    is_integer(MaxRetry) andalso MaxRetry > 0 orelse error(badarg),
+    #spinlock{
+        ref = atomics:new(2, [{signed, false}]),
+        max_retry = MaxRetry
+    }.
+
+-spec acquire(#spinlock{}) -> lock_id().
+acquire(Lock = #spinlock{ref = Ref}) ->
+    LockId = atomics:add_get(Ref, 1, 1),
+    spin(Lock, LockId, undefined, 0).
+
+-spec release(#spinlock{}, lock_id()) -> ok | {error, already_released | invalid_lock_id}.
+release(#spinlock{ref = Ref}, LockId) ->
+    case atomics:compare_exchange(Ref, 2, LockId - 1, LockId) of
+        ok -> ok;
+        Id when Id >= LockId -> {error, already_released};
+        _ -> {error, invalid_lock_id}
+    end.
+
+-spec transaction(#spinlock{}, fun(() -> any())) -> any().
+transaction(Lock, Fun) ->
+    LockId = acquire(Lock),
+    try Fun() of
+        Res -> Res
+    after
+        release(Lock, LockId)
+    end.
+
+-spec status(#spinlock{}) ->
+    #{released => non_neg_integer(), is_locked => boolean(), waiting => non_neg_integer()}.
+status(#spinlock{ref = Ref}) ->
+    Released = atomics:get(Ref, 2),
+    Total = atomics:get(Ref, 1),
+    #{
+        released => Released,
+        is_locked => Total > Released,
+        waiting => max(Total - Released - 1, 0)
+    }.
+
+spin(Lock = #spinlock{ref = Ref, max_retry = MaxRetry}, LockId, Released, MaxRetry) ->
+    atomics:compare_exchange(Ref, 2, Released, Released + 1),
+    spin(Lock, LockId, Released, 0);
+spin(Lock = #spinlock{ref = Ref}, LockId, LastReleased, Retry) ->
+    case atomics:get(Ref, 2) of
+        LastReleased ->
+            LastReleased < LockId - 2 andalso erlang:yield(),
+            spin(Lock, LockId, LastReleased, Retry + 1);
+        Released when Released >= LockId - 1 ->
+            LockId;
+        Released ->
+            Released < LockId - 2 andalso erlang:yield(),
+            spin(Lock, LockId, Released, 0)
+    end.
